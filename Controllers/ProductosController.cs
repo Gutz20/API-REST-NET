fe2deced1597ed71f7e58668e409e68a2cfbad99
Guz <darckproyect8@gmail.com>
2023-04-30 20:17:25 -0500

    Creating a API REST basic

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tiendaapi.Datos;
+using Tiendaapi.Modelo;
+
+namespace Tiendaapi.Controllers
+{
+    [ApiController]
+    [Route("api/productos")]
+    public class ProductosController:ControllerBase
+    {
+        [HttpGet]
+        public async Task <ActionResult<List<MProductos>>> Get()
+        {
+            var funcion = new DProductos();
+            var lista = await funcion.MostrarProductos();
+            return lista;
+        }
+        [HttpPost]
+        public async Task Post([FromBody] MProductos parametros)
+        {
+            var funcion = new DProductos();
+            await funcion.InsertarProductos(parametros);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] MProductos parametros)
+        {
+            var funcion = new DProductos();
+            parametros.id = id;
+            await funcion.EditarProductos(parametros);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var funcion = new DProductos();
+            var parametros = new MProductos();
+            parametros.id = id;
+            await funcion.EliminarProductos(parametros);
+            return NoContent();
+        }
+    }
+}
---
+++ Datos/DProductos.cs
@@ -0,0 +1,76 @@
+﻿using System.Data.SqlClient;
+using Tiendaapi.Conexion;
+using Tiendaapi.Modelo;
+using System.Data;
+
+namespace Tiendaapi.Datos
+{
+    public class DProductos
+    {
+        Conexionbd cn = new Conexionbd();
+        public async Task <List<MProductos>> MostrarProductos() {
+            var lista = new List<MProductos>();
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("mostrarProductos", sql))
+                {
+                    await sql.OpenAsync();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (var item = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await item.ReadAsync())
+                        {
+                            var mproductos = new MProductos();
+                            mproductos.id = (int)item["id"];
+                            mproductos.descripcion = (string)item["descripcion"];
+                            mproductos.precio = (decimal)item["precio"];
+                            lista.Add(mproductos);
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+        public async Task InsertarProductos(MProductos parametros)
+        {
+            using(var sql = new SqlConnection (cn.cadenaSQL()))
+            {
+                using(var cmd = new SqlCommand("insertarProductos", sql))
+                {
+                    cmd.CommandType=CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@descripcion", parametros.descripcion);
+                    cmd.Parameters.AddWithValue("@precio", parametros.precio);
+                    await sql.OpenAsync() ;
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+        public async Task EditarProductos(MProductos parametros)
+        {
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("editarProductos", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@id", parametros.id);
+                    cmd.Parameters.AddWithValue("@precio", parametros.precio);
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+        public async Task EliminarProductos(MProductos parametros)
+        {
+            using(var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using(var cmd = new SqlCommand("eliminarProductos", sql))
+                {
+                    cmd.CommandType=CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("id", parametros.id);
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+    }
+}
